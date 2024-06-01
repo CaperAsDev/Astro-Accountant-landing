35b2de7e10a76b51363f5ffaea5aba77cb30800f
CaperAsDev <caper.asdev@gmail.com>
2024-06-01 12:03:52 -0500

    eager loading for images and compress added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), playformCompress()],
   base: process.env.SERVER === 'pre-production' ? `/${process.env.REPO}` : '/',
   build: {
     assets: 'assets'
