351eb4032593e9d89083687720abacffee391a8f
CaperAsDev <caper.asdev@gmail.com>
2024-06-07 11:34:30 -0500

    Seo initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import playformCompress from "@playform/compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), playformCompress()],
+  site: 'https://excellentiaconsultingbpo.com',
+  integrations: [tailwind(), icon(), playformCompress(), sitemap()],
   base: process.env.SERVER === 'pre-production' ? `/${process.env.REPO}` : '/',
   build: {
     assets: 'assets'
